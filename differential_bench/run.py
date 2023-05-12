35710b22f20b5b63fefe98e23a37f0d63d178de8
Joshua Schneider <joshua.schneider@inf.ethz.ch>
2023-05-12 13:38:10 +0200

    differential/batching benchmark: ONCE P AND NOT ONCE Q

--- differential_bench/run.py
+++ differential_bench/run.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 # Parameters
 SEED = 20230508
 REP = 3
-TOTAL_EVENTS = 20_000
-TP_PER_TS = [5, 10, 15, 20]
-EV_PER_TP = 10
+TOTAL_EVENTS = [2_000, 4_000, 6_000, 8_000]
+TP_PER_TS = 10
+EV_PER_TP = 2
 MAX_VALUE = 1_000_000_000
 PROGRAMS = ["no_batching", "batching", "batching_new"]
 BATCH_SIZES = [1, 5]
@@ -16,14 +16,14 @@ random.seed(SEED)
 
 Trace = list[tuple[int, list[tuple[str, int]]]]
 
-def input_generator(tp_per_ts: int) -> Trace:
-    num_ts = round(TOTAL_EVENTS / 2 / EV_PER_TP / tp_per_ts)
+def input_generator(total_events: int) -> Trace:
+    num_ts = round(total_events / 2 / EV_PER_TP / TP_PER_TS)
     events = []
     cache = []
     count = 0
     num_tp = 0
     for ts in range(num_ts):
-        for tp in range(tp_per_ts):
+        for tp in range(TP_PER_TS):
             curr_events = [("Q", elem) for elem in cache]
             cache.clear()
             for _ in range(EV_PER_TP):
@@ -47,10 +47,10 @@ def file_generator(events: Trace, batch_size: int, filename: str):
                     file.write("insert " + rel + "(" + str(tp) + ", " + str(elem) + ");\n")
             file.write("commit dump_changes;\n\n")
 
-def measure(program: str, tp_per_ts: int, batch_size: int) -> float:
-    print(f"{program}, batch size {batch_size}, {tp_per_ts} tp/ts")
+def measure(program: str, total_events: int, batch_size: int) -> float:
+    print(f"{program}, batch size {batch_size}, {total_events} events")
     INPUT_FILE = 'input.txt'
-    events = input_generator(tp_per_ts)
+    events = input_generator(total_events)
     file_generator(events, batch_size, INPUT_FILE)
     args = ["/usr/bin/time", "-f", "%e", f"./{program}_ddlog/target/release/{program}_cli"]
     acc = 0.0
@@ -66,14 +66,14 @@ def measure(program: str, tp_per_ts: int, batch_size: int) -> float:
     return avg
 
 def measure_series(program: str, batch_size: int) -> list[float]:
-    return [measure(program, rate, batch_size) for rate in TP_PER_TS]
+    return [measure(program, x, batch_size) for x in TOTAL_EVENTS]
 
 def main():
     for program in PROGRAMS:
         bsz = [1] if program == "no_batching" else BATCH_SIZES
         for batch_size in bsz:
             label = f"{program}, batch size {batch_size}"
-            plt.plot(TP_PER_TS, measure_series(program, batch_size), label=label)
+            plt.plot(TOTAL_EVENTS, measure_series(program, batch_size), label=label)
     plt.xlabel("tp/ts")
     plt.ylabel("runtime [s]")
     plt.figlegend()
